2524246a1579ad133aaff9342761d2a99796fe25
C3 <letterdc749@gmail.com>
2026-05-09 11:34:01 -0700

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -27,7 +27,7 @@ export const botConfig = {
         // Text users will see (example: "Playing /help | Titan Bot").
         name: "Made by 7allurez",
         // Activity type number (0 = Playing).
-        type: 0, 
+        type: 3, 
       },
     ],
   },
