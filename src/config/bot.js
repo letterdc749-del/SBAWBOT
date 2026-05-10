fba1b962de0a54a2b413800c7a4f7917c9f3e15d
C3 <letterdc749@gmail.com>
2026-05-10 16:02:26 -0700

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -25,7 +25,7 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: "South Bronx: Apex",
+        name: "North Chicago",
         // Activity type number (0 = Playing).
         type: 0, 
       },
